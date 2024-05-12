4b08a75f7d3de57e105d5ec86c828d79a180623e
Christopher Trimboli <cjft@trimboli.io>
2024-05-12 00:25:59 -0600

    add grid metaverse floor

--- components/Scene.tsx
+++ components/Scene.tsx
@@ -1,4 +1,5 @@
 import { Canvas } from "@react-three/fiber/native";
+import { PerspectiveCamera, Plane } from "@react-three/drei/native";
 import VrmAvatar from "./VrmAvatar";
 import { Matrix4, Vector3, Quaternion, Euler, MathUtils } from "three";
 import { defaultAnimations } from "../constants/animations";
@@ -7,21 +8,21 @@ export default function App() {
   return (
     <>
       <Canvas>
-        <ambientLight intensity={0.5} />
-        <spotLight
-          position={[10, 10, 10]}
-          angle={0.15}
-          penumbra={1}
-          decay={0}
-          intensity={1}
+        <PerspectiveCamera
+          makeDefault
+          position={[0, 0.3, 1.75]}
+          rotation={[-Math.PI / 12, 0, 0]}
         />
-        <pointLight position={[-10, -10, -10]} decay={0} intensity={1} />
+
+        <ambientLight intensity={0.7} />
+
+        <pointLight position={[0, -10, -10]} decay={0} intensity={1} />
 
         <VrmAvatar
           matrix={new Matrix4().compose(
-            new Vector3(0, -0.8, 3.8),
+            new Vector3(0, -1, 0),
             new Quaternion().setFromEuler(
-              new Euler(MathUtils.degToRad(5), MathUtils.degToRad(180), 0)
+              new Euler(0, MathUtils.degToRad(180), 0)
             ),
             new Vector3(1, 1, 1)
           )}
@@ -30,6 +31,16 @@ export default function App() {
           audioRef={undefined}
           scale={[1, 1, 1]}
         />
+
+        <color attach="background" args={["#141414"]} />
+
+        <Plane
+          args={[20, 20, 20, 20]}
+          position={[0, -1, 0]}
+          rotation={[Math.PI / 2, 0, 0]}
+        >
+          <meshStandardMaterial color="#292929" wireframe />
+        </Plane>
       </Canvas>
     </>
   );
